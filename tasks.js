3056f04b6fb7e57d0273cc71323e91189d6f50af
00kae <nguyenmisha00@gmail.com>
2025-12-12 16:49:44 -0800

    commit 3

--- tasks.js
+++ tasks.js
@@ -1,7 +1,9 @@
 
 const MAX_TASK = 50;
 // maximum task number 
+
 const task_list = new Map();
+// user task list
 
 class Task {
     constructor(position, task) {
@@ -10,15 +12,20 @@ class Task {
   }
 }
 
+// var input should be in add_task ?? like the if keypress == enter
+// task_box.addEventListener("click", add_task);
+// var input = landing.getElementbyId("myInput")
+// user keypress
 
 
 function add_task(){
-    // eventlistener -> task text box is clicked, then ask for plaintext
+    // eventlistener -> task text box is clicked, then ask for plaintext (will be in html)
     // if enter is pressed and task is filled with more than 1 char, then make a new line and add
     // task to list 
         // elseif clicked outside, then close new empty line
     // use a hashmap and also store the position???
     task_list.set(Task.position, Task.task);
+    // then task is set to position (redundant.. but could work)
 
 };
 function remove_task(){};
